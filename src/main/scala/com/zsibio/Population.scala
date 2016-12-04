40a15889a02419cf91d2bcbd2c3e01b61c18921d
AnastasiiaInna <hryhorzhevskaia@gmail.com>
2016-12-04 19:37:45 +0100

    Update

--- src/main/scala/com/zsibio/Population.scala
+++ src/main/scala/com/zsibio/Population.scala
@@ -1,26 +1,27 @@
 package com.zsibio
 
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{SparkContext}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
 import org.apache.spark.sql.types._
-import org.bdgenomics.adam.rdd.ADAMContext._
 import org.bdgenomics.formats.avro.{Genotype, GenotypeAllele}
 
 import scala.collection.JavaConverters._
-import java.nio.file.{Files, Paths}
 
 case class SampleVariant(sampleId: String, variantId: String, alternateCount: Int)
 
 trait PopulationMethods{
   def getDataSet(dataSet: RDD[(String, Array[SampleVariant])], prunnedSnpIdSet: List[String]): DataFrame
+  def getDataSet(dataSet: RDD[(String, Array[Double])]): DataFrame
 }
 
 @SerialVersionUID(15L)
-class Population[T] (sc: SparkContext, sqlContext: SQLContext, genotypes: RDD[Genotype], panel: Map[String, String], var infFreq: Double = 0.05, var supFreq: Double = 1.0) extends Serializable with PopulationMethods{
+class Population[T] (sc: SparkContext, sqlContext: SQLContext, genotypes: RDD[Genotype], panel: Map[String, String],
+                     val missingRate: Double = 0.0, var infFreq: Double = 0.05, var supFreq: Double = 1.0) extends Serializable with PopulationMethods{
 
-  private var _infFreq : Double = infFreq
-  private var _supFreq : Double = supFreq
+  private var _infFreq     : Double = infFreq
+  private var _supFreq     : Double = supFreq
+  private val _missingRate : Double = missingRate
   var _dataSet: DataFrame = null
   var _numberOfRegions : Int = 0
 
@@ -34,12 +35,12 @@ class Population[T] (sc: SparkContext, sqlContext: SQLContext, genotypes: RDD[Ge
     s"$name:$start:$end"
   }
 
-  private def alternateCount(genotype: Genotype): Int = {
+  private def altCount(genotype: Genotype): Int = {
     genotype.getAlleles.asScala.count(_ != GenotypeAllele.Ref)
   }
 
   private def toVariant(genotype: Genotype): SampleVariant = {
-    new SampleVariant(genotype.getSampleId.intern(), variantId(genotype), alternateCount(genotype))
+    new SampleVariant(genotype.getSampleId.intern(), variantId(genotype), altCount(genotype))
   }
 
   val variantsRDD: RDD[SampleVariant] = genotypes.map(toVariant)
@@ -49,40 +50,33 @@ class Population[T] (sc: SparkContext, sqlContext: SQLContext, genotypes: RDD[Ge
   val sampleCount: Long = variantsBySampleId.count()
 
   private val variantsByVariantId: RDD[(String, Iterable[SampleVariant])] = variantsRDD.groupBy(_.variantId).filter {
-    case (_, sampleVariants) => sampleVariants.size == sampleCount
+    case (_, sampleVariants) => sampleVariants.size <= (sampleCount * (1 + _missingRate)).toInt
   }
 
-  private val variantFrequencies: Map[String, Double] = variantsByVariantId.map {
+  private val variantFrequencies: RDD[(String, Double)] = variantsByVariantId.map {
     case (variantId, sampleVariants) => (variantId, sampleVariants.count(_.alternateCount > 0) / sampleCount.toDouble)
-  }.collect().toMap
-
-  val sortedVariantsBySampelId: RDD[(String, Array[SampleVariant])] ={
-    val filteredVariantsBySampleId: RDD[(String, Iterable[SampleVariant])] = variantsBySampleId.map {
-      case (sampleId, sampleVariants) =>
-        val filteredSampleVariants = sampleVariants.filter(variant => variantFrequencies.getOrElse(variant.variantId, -1.0) >= _infFreq
-          && variantFrequencies.getOrElse(variant.variantId, -1.0) <= _supFreq)
-        (sampleId, filteredSampleVariants)
-    }
-
-    filteredVariantsBySampleId.map {case (sampleId, variants) => (sampleId, variants.toArray.sortBy(_.variantId)) }
   }
 
-   def getDataSet(sortedVariantsBySampleId: RDD[(String, Array[SampleVariant])] = sortedVariantsBySampelId, prunnedSnpIdSet: List[String] = null) : DataFrame ={
+  private val frequencies : Array[String] = if (_infFreq == .0 && _supFreq == 1.0) variantFrequencies.keys.collect else variantFrequencies.filter{case(_, freq) => freq >= _infFreq && freq <= _supFreq}.keys.collect()
+  private val filteredVariants: RDD[(String, Iterable[SampleVariant])] = variantsRDD.filter{sampleVariant => frequencies contains(sampleVariant.variantId)}.groupBy(_.sampleId).cache()
+  val sortedVariantsBySampelId: RDD[(String, Array[SampleVariant])] = filteredVariants.map{case (sampleId, sampleVariant) => (sampleId, sampleVariant.toArray.sortBy(_.variantId))}.cache()
+
+  def getDataSet(sortedVariantsBySampleId: RDD[(String, Array[SampleVariant])] = sortedVariantsBySampelId, prunnedSnpIdSet: List[String] = null) : DataFrame ={
      val header = StructType(
       Array(StructField("SampleId", StringType)) ++
         Array(StructField("Region", StringType)) ++
         sortedVariantsBySampleId.first()._2.map(variant => {
-          StructField(variant.variantId.toString, IntegerType)
+          StructField(variant.variantId.toString, DoubleType)
         }))
 
      val rowRDD: RDD[Row] = sortedVariantsBySampleId.map {
        case (sampleId, sortedVariants) =>
          val region: Array[String] = Array(panel.getOrElse(sampleId, "Unknown"))
-         val alternateCounts: Array[Int] = sortedVariants.map(_.alternateCount)
+         val alternateCounts: Array[Double] = sortedVariants.map(_.alternateCount.toDouble)
          Row.fromSeq(Array(sampleId) ++ region ++ alternateCounts)
      }
 
-     var dataSet: DataFrame = sqlContext.createDataFrame(rowRDD, header)
+     val dataSet: DataFrame = sqlContext.createDataFrame(rowRDD, header)//.toDF(header.fieldNames : _*)
      var outputDataSet: DataFrame = null
 
      if (prunnedSnpIdSet == null) outputDataSet = dataSet
@@ -97,8 +91,34 @@ class Population[T] (sc: SparkContext, sqlContext: SQLContext, genotypes: RDD[Ge
      return outputDataSet
   }
 
+  def getDataSet(variants: RDD[(String, Array[Double])]) : DataFrame ={
+    val pc = Array.fill(variants.first()._2.length)("PC")
+    val s = (1 until variants.first()._2.length)
+    val headerPC = (pc, s).zipped.par.map{case(pc, s) => pc + s}
+
+    val header = StructType(
+      Array(StructField("SampleId", StringType)) ++
+        Array(StructField("Region", StringType)) ++
+        headerPC.map(pc => {StructField(pc, DoubleType)})
+    )
+
+    val rowRDD: RDD[Row] = variants.map {
+      case (sampleId, variant) =>
+        val region: Array[String] = Array(panel.getOrElse(sampleId, "Unknown"))
+        Row.fromSeq(Array(sampleId) ++ region ++ variant)
+    }
+
+    val outputDataSet: DataFrame = sqlContext.createDataFrame(rowRDD, header)//.toDF(header.fieldNames : _*)
+
+    _dataSet = outputDataSet
+    _numberOfRegions = outputDataSet.select("Region").distinct().count().toInt
+
+    return outputDataSet
+  }
+
 }
 
 object Population {
-  def apply[T](sc: SparkContext, sqlContext: SQLContext, genotypes: RDD[Genotype], panel: Map[String, String], infFreq: Double,  supFreq: Double): Population[T] = new Population[T](sc, sqlContext, genotypes, panel, infFreq, supFreq)
+  def apply[T](sc: SparkContext, sqlContext: SQLContext, genotypes: RDD[Genotype], panel: Map[String, String],
+               missingRate: Double, infFreq: Double,  supFreq: Double): Population[T] = new Population[T](sc, sqlContext, genotypes, panel, missingRate, infFreq, supFreq)
 }
